50511aa4a6d7814e593f7767e91fd7f8ed71bebf
Fynn Becker <post@fynn.be>
2019-08-06 11:47:33 +0200

    fix: Don't add badge if it has already been added
    This circumvents multiple badge instances on SPAs.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 import './main.css'
 
-const HREF = 'https://www.abendblatt.de/hamburg/article226649773/Abendblatt-startet-Aktion-fuer-mehr-Mitmenschlichkeit.html'
-const ALT = 'Seid nett zueinander (Hamburger Abendblatt)'
+const LINK = 'https://www.abendblatt.de/hamburg/article226649773/Abendblatt-startet-Aktion-fuer-mehr-Mitmenschlichkeit.html'
+const LINK_CLASS = 'seid-nett-zueinander-link'
+const IMAGE_CLASS = 'seid-nett-zueinander-image'
+const IMAGE_ALT = 'Seid nett zueinander (Hamburger Abendblatt)'
 
-const link = document.createElement('a')
-link.href = HREF
-link.classList.add('seid-nett-zueinander-link')
+if (!document.querySelector(`.${LINK_CLASS}`)) {
+  const link = document.createElement('a')
+  link.href = LINK
+  link.classList.add(LINK_CLASS)
 
-const image = document.createElement('img')
-image.src = require('./badge.svg')
-image.alt = ALT
-image.classList.add('seid-nett-zueinander-image')
+  const image = document.createElement('img')
+  image.src = require('./badge.svg')
+  image.alt = IMAGE_ALT
+  image.classList.add(IMAGE_CLASS)
 
-link.appendChild(image)
-
-document.body.appendChild(link)
+  link.appendChild(image)
+  document.body.appendChild(link)
+}
