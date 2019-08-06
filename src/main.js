2edf27ef865b06897eeb33ac10b313150f4c4d1a
Fynn Becker <post@fynn.be>
2019-08-06 09:45:59 +0200

    fix: Remove blank link target
    If the link is opened in a new tab, the focus on the previous page remains active and the badge stays floating around.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import './main.css'
 
 const HREF = 'https://www.abendblatt.de/hamburg/article226649773/Abendblatt-startet-Aktion-fuer-mehr-Mitmenschlichkeit.html'
-const TARGET = '_blank'
 const ALT = 'Seid nett zueinander (Hamburger Abendblatt)'
 
 const link = document.createElement('a')
 link.href = HREF
-link.target = TARGET
 link.classList.add('seid-nett-zueinander-link')
 
 const image = document.createElement('img')
